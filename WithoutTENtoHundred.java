3e641e62dbec788dca85bfd014467ae2f88f614a
ShaileshWattamwar <144618665+ShaileshWattamwar@users.noreply.github.com>
2023-10-05 15:05:24 +0530

    Add files via upload

---
+++ WithoutTENtoHundred.java
@@ -0,0 +1,12 @@
+package TDIT_Ass_forloop;
+
+public class WithoutTENtoHundred {
+    public static void main(String[] args) {
+        for (int i = 0; i <100 ; i++) {
+            if (i%10!=0){
+                System.out.println(i);
+            }
+        }
+
+    }
+}
---
+++ WithoutTwo.java
@@ -0,0 +1,13 @@
+package TDIT_Ass_forloop;
+
+public class WithoutTwo {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 10; i++) {
+
+            if (i != 2) {
+                System.out.println(i);
+            }
+        }
+    }
+}
